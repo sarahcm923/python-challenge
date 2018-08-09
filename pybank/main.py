8b4a24b9c6acfd9b70082f15293f6c2de3d95d24
S Steimle <sarah.mullet@gmail.com>
2018-08-09 17:55:24 -0400

    homework

---
+++ pybank/main.py
@@ -0,0 +1,25 @@
+month_counter = 0
+profit_increase = 0
+profit_decrease = 0
+net_profit_loss = 0
+average_change = 0.00
+
+# loop through rows
+
+for i in range(x):
+    month_counter = month_counter + 1
+
+    if (cell - previous cell > profit_increase):
+        profit_increase = cell - previous cell
+        else:
+            
+    if (profit_decrease > cell - previous cell):
+        profit_decrease = cell - previous cell
+        else:
+
+    net_profit_loss = net_profit_loss + current cell
+
+average_change = (net_profit_loss / month_counter)
+
+print (blah blah)
+export to file
